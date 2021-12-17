b1a710a0c6f33b5f2fa52fc72820cbab5a42032d
kreichjr <kreichjr@gmail.com>
2021-12-16 21:49:53 -0600

    Heroku Deployment part 2

--- run_the_set_backend/settings.py
+++ run_the_set_backend/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+print(os.environ)
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
@@ -28,6 +32,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG") == 'TRUE'
 
 ALLOWED_HOSTS = []
+
+# Deployment Settings
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 
 # Application definition
@@ -43,7 +51,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'players',
     'characters',
-    'matches',
+    'matches'
 
 ]
 
@@ -136,4 +144,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
 django_heroku.settings(locals())
--- run_the_set_backend/wsgi.py
+++ run_the_set_backend/wsgi.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
 
-project_folder = os.path.expanduser('~/my-project-dir')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
+from dotenv import load_dotenv
+load_dotenv()
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'run_the_set_backend.settings')
+
 
 application = get_wsgi_application()
